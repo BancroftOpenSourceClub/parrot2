6d1eda5fb0c8d807ed51514939352e52e662fbbb
partavocado <chuenhei123@gmail.com>
2024-10-31 12:03:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,13 +30,22 @@ let flipped;
 });
 
 const colorsOptions = [
-  'red',
-  'yellow',
-  'green',
-  'blue',
-  'magenta',
-  'cyan',
-  'white'
+  'Aqua',
+  'Black',
+  'Blue',
+  'Fuchsia',
+  'Gray',
+  'Green',
+  'Lime',
+  'Maroon',
+  'Navy',
+  'Olive',
+  'Purple',
+  'Red',
+  'Silver',
+  'Teal',
+  'White',
+  'Yellow'
 ];
 const numColors = colorsOptions.length;
 const selectColor = previousColor => {
